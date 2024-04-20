894fea23e0fe2e27f2ce2ed582f8ee6be6587da9
Tham <thamdavies@gmail.com>
2024-04-20 09:35:07 +0700

    updated Cloak keymap

--- nvim/lua/ngaicojal/remap.lua
+++ nvim/lua/ngaicojal/remap.lua
@@ -51,8 +51,7 @@ map("n", "<leader>fo", "<cmd>Telescope oldfiles<CR>", { desc = "Telescope Find o
 map("n", "<leader>fz", "<cmd>Telescope current_buffer_fuzzy_find<CR>", { desc = "Telescope Find in current buffer" })
 map("n", "<leader>cm", "<cmd>Telescope git_commits<CR>", { desc = "Telescope Git commits" })
 map("n", "<leader>gt", "<cmd>Telescope git_status<CR>", { desc = "Telescope Git status" })
-map("n", "<leader>pt", "<cmd>Telescope terms<CR>", { desc = "Telescope Pick hidden term" })
-map("n", "<leader>th", "<cmd>Telescope themes<CR>", { desc = "Telescope Nvchad themes" })
+map("n", "<leader>ct", "<cmd>CloakToggle<CR>", { desc = "Cloak toggle" })
 map(
   "n",
   "<leader>fa",
