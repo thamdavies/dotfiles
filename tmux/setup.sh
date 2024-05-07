d0599f5a762ed47bf8a5d127d48030436e8ed3f1
Tham <tham@tomosia.com>
2024-05-07 10:20:11 +0700

    added tmux plugins

---
+++ tmux/setup.sh
@@ -0,0 +1,3 @@
+mkdir -p "$XDG_CONFIG_HOME/tmux"
+ln -s ~/personal/dotfiles/tmux/tmux.conf "$XDG_CONFIG_HOME/tmux/tmux.conf"
+ln -s ~/personal/dotfiles/tmux/tmux.conf.local "$XDG_CONFIG_HOME/tmux/tmux.conf.local"
