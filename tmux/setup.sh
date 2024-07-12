b1047686241af45921388a9bd9f7fb377cdfa56a
Tham <thamdavies@gmail.com>
2024-07-12 13:19:12 +0700

    Update tmux config file

--- tmux/setup.sh
+++ tmux/setup.sh
@@ -1,6 +1,6 @@
-mkdir -p "$XDG_CONFIG_HOME/tmux"
-rm -rf "$XDG_CONFIG_HOME/tmux/tmux.conf"
-rm -rf "$XDG_CONFIG_HOME/tmux/tmux.conf.local"
+mkdir -p "$HOME/.config/tmux"
+rm -rf "$HOME/.config/tmux/tmux.conf"
+rm -rf "$HOME/.config/tmux/tmux.conf.local"
 
-ln -s ~/personal/dotfiles/tmux/tmux.conf "$XDG_CONFIG_HOME/tmux/tmux.conf"
-ln -s ~/personal/dotfiles/tmux/tmux.conf.local "$XDG_CONFIG_HOME/tmux/tmux.conf.local"
+ln -s ~/personal/dotfiles/tmux/tmux.conf "$HOME/.config/tmux/tmux.conf"
+ln -s ~/personal/dotfiles/tmux/tmux.conf.local "$HOME/.config/tmux/tmux.conf.local"
