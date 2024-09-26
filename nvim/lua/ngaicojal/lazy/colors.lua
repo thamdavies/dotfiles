8a1b6213c066789664759d559e364b19590a60e2
Tham <tham@tomosia.com>
2024-09-26 10:12:18 +0700

    Update

--- nvim/lua/ngaicojal/lazy/colors.lua
+++ nvim/lua/ngaicojal/lazy/colors.lua
@@ -1,14 +1,21 @@
+-- return {
+--   {
+--     "rose-pine/neovim",
+--     name = "rose-pine",
+--     config = function()
+--       require("rose-pine").setup({
+--         variant = "moon", -- auto, main, moon, or dawn
+--         dark_variant = "moon", -- main, moon, or dawn
+--       })
+--
+--       vim.cmd("colorscheme rose-pine-moon")
+--     end
+--   },
+-- }
 return {
-  {
-    "rose-pine/neovim",
-    name = "rose-pine",
-    config = function()
-      require("rose-pine").setup({
-        variant = "moon", -- auto, main, moon, or dawn
-        dark_variant = "moon", -- main, moon, or dawn
-      })
-
-      vim.cmd("colorscheme rose-pine-moon")
-    end
-  },
+  "navarasu/onedark.nvim",
+  name = "onedark",
+  config = function()
+    require("onedark").load()
+  end
 }
