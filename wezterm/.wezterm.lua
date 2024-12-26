d799c4066fc695514fcd1e75581c8f4178b13b4e
Tham <thamdavies@gmail.com>
2024-12-26 09:11:04 +0700

    add wezterm and aerospace config

---
+++ wezterm/.wezterm.lua
@@ -0,0 +1,28 @@
+-- Pull in the wezterm API
+local wezterm = require 'wezterm'
+
+-- This will hold the configuration.
+local config = wezterm.config_builder()
+local home_dir = os.getenv("HOME")
+
+-- This is where you actually apply your config choices
+
+-- For example, changing the color scheme:
+config.font = wezterm.font 'JetBrainsMono Nerd Font'
+config.font_size = 12.5
+
+config.window_background_image = home_dir .. '/.config/nvim/bg/1.jpg'
+config.enable_tab_bar = false
+
+config.window_decorations = "RESIZE"
+config.window_background_opacity = 0.9
+config.macos_window_background_blur = 10
+config.window_background_image_hsb = {
+  hue = 1.0,
+  saturation = 0.7,
+  brightness = 0.03,
+}
+
+-- and finally, return the configuration to wezterm
+return config
+
