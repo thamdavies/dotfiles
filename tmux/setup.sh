61e2a142ade867ee527bfb998e7bb14446c1f314
Tham <thamdavies@gmail.com>
2024-12-29 18:34:59 +0700

    reconfig tmux

--- tmux/setup.sh
+++ tmux/setup.sh
@@ -1,6 +1,1 @@
-mkdir -p "$HOME/.config/tmux"
-rm -rf "$HOME/.config/tmux/tmux.conf"
-rm -rf "$HOME/.config/tmux/tmux.conf.local"
-
-ln -s ~/personal/dotfiles/tmux/tmux.conf "$HOME/.config/tmux/tmux.conf"
-ln -s ~/personal/dotfiles/tmux/tmux.conf.local "$HOME/.config/tmux/tmux.conf.local"
+stow -v -t ~ tmux
