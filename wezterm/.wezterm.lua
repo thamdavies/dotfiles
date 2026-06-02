97460cabb541f924914ffd619998c076ec744a2a
Tham <thamdavies@gmail.com>
2026-06-02 07:12:20 +0700

    Update wezterm font configuration and modify zsh environment variables

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -8,7 +8,7 @@ local home_dir = os.getenv("HOME")
 -- This is where you actually apply your config choices
 
 config.font_size = 15
-config.font = wezterm.font { family = 'Maple Mono NF' }
+config.font = wezterm.font { family = 'JetBrainsMono Nerd Font' }
 
 local background_image_path = os.getenv("WEZTERM_BG_IMAGE") or (home_dir .. '/personal/dotfiles/images/1.jpg')
 config.window_background_image = background_image_path
